f1fa769fbeb335be3f9c7a655849ad7bbb72ab31
Вячеслав Трендев <trendev97@gmail.com>
2023-05-01 16:33:14 +0500

    Написал программу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿string[] text = { "Hello", "!", "Geek", "Brains", "=D", "123" };
+
+int size = Count(text);
+string[] newText = new string[size];
+Print(FillArray(text, newText));
+
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+string[] FillArray(string[] array, string[] newArray)
+{
+    int n = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[n] = array[i];
+            n++;
+        }
+    }
+    return newArray;
+}
+
+int Count(string[] str)
+{
+    int res = 0;
+
+    for (int i = 0; i < str.Length; i++)
+        if (str[i].Length >= 3) res++;
+
+    return res;
+}
+
+
